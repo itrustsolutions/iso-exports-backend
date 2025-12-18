c577bd8c7d2150ca01cf617cf866d692903f90e5
Rami Rayya <rami.k.rayya@gmail.com>
2025-12-19 02:31:38 +0300

    feat(utils/errors): add business logic custom error struct & helpers

---
+++ utils/errors/business/business.errors.go
@@ -0,0 +1,50 @@
+package businesserrors
+
+import "fmt"
+
+type BusinessError struct {
+	Code    string                 `json:"code"`
+	Message string                 `json:"message"`
+	Details map[string]interface{} `json:"details,omitempty"`
+	Err     error                  `json:"-"` // Underlying error, not exposed in JSON response
+}
+
+func (e *BusinessError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s: %s (underlying: %v)", e.Code, e.Message, e.Err)
+	}
+	return fmt.Sprintf("%s: %s", e.Code, e.Message)
+}
+
+func (e *BusinessError) Unwrap() error {
+	return e.Err
+}
+
+func NewBusinessError(code, message string) *BusinessError {
+	return &BusinessError{
+		Code:    code,
+		Message: message,
+		Details: make(map[string]interface{}),
+	}
+}
+
+func NewValidationError(errors map[string][]string) *BusinessError {
+	// Convert map[string][]string to map[string]interface{}
+	details := make(map[string]interface{})
+	for key, value := range errors {
+		details[key] = value
+	}
+
+	return NewBusinessError(ErrCodeInvalidJSONInput, "Invalid input").
+		WithDetails(details)
+}
+
+func (e *BusinessError) WithDetails(details map[string]interface{}) *BusinessError {
+	e.Details = details
+	return e
+}
+
+func (e *BusinessError) WithError(err error) *BusinessError {
+	e.Err = err
+	return e
+}
---
+++ utils/errors/business/codes.errors.go
@@ -0,0 +1,10 @@
+package businesserrors
+
+// Common business error codes
+const (
+	ErrCodeValidationFailed = "COMMON.VALIDATION_FAILED"
+	ErrCodeInvalidJSONInput = "COMMON.INVALID_JSON_INPUT"
+	ErrCodeUnauthorized     = "COMMON.UNAUTHORIZED"
+	ErrCodeForbidden        = "COMMON.FORBIDDEN"
+	ErrCodeInternalError    = "COMMON.INTERNAL_ERROR"
+)
